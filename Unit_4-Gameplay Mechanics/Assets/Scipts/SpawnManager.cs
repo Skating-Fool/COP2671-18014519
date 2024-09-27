8c241470b329025e8e26b1c8b98567e252714f90
Skating-Fool <106551752+Skating-Fool@users.noreply.github.com>
2024-09-26 21:20:27 -0400

    Lesson 4.2 Done

---
+++ Unit_4-Gameplay Mechanics/Assets/Scipts/SpawnManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject enemyPrefab;
+    private float spawnRange = 9;
+
+    void Start()
+    {
+        Instantiate(enemyPrefab, GenerateSpawnPosoton(), enemyPrefab.transform.rotation);
+    }
+
+    private Vector3 GenerateSpawnPosoton()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosY = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosY);
+        return randomPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
