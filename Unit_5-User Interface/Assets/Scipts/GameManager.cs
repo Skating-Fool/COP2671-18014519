73e20715daef371e5399b82037cebff03aaef7d9
Skating-Fool <106551752+Skating-Fool@users.noreply.github.com>
2024-10-03 20:37:56 -0400

    Lesson 5.3 Done

--- Unit_5-User Interface/Assets/Scipts/GameManager.cs
+++ Unit_5-User Interface/Assets/Scipts/GameManager.cs
@@ -2,27 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
 
     public TextMeshProUGUI scoreText;
-    private int score;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
 
     public List<GameObject> targets;
+
+    public bool isGameActive;
+
+    private int score;
 
     private float spawnRate = 1.0f;
 
     void Start()
     {
+        isGameActive = true;
         UpdateScore(0);
         StartCoroutine(SpawnTarget());
     }
 
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
@@ -34,6 +49,11 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     // Update is called once per frame
