328f33d61114ac348adcdc77983c6137695885a4
Skating-Fool <106551752+Skating-Fool@users.noreply.github.com>
2024-11-08 06:29:58 -0500

    more gamemanager stuff

--- Final Project/REACT/Assets/Scripts/GameManager.cs
+++ Final Project/REACT/Assets/Scripts/GameManager.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
-    public int wave;
+    public bool waveActive;
+    public int wave = 1;
     public float waveTime;
     public float waveScale;
 
-    public int[] keyWaves;
+    public int keyWaveInterval = 5;
+    public int startingDifficulty;
+    public int difficulty;
 
     public TrackSpawner[] spawners;
 
     private int enemyCount;
-
 
     void Start()
     {
@@ -23,9 +25,26 @@ public class GameManager : MonoBehaviour
     
     void Update()
     {
-        
+        if (!waveActive)
+        {
+            StartWave();
+        }
     }
 
+    public void StartWave(int? waveOverride = null)
+    {
 
+        if (waveOverride != null)
+        {
+            wave = waveOverride.Value;
+        }
+        else
+        {
+            wave++;
+        }
+
+        waveActive = true;
+
+    }
 
 }
